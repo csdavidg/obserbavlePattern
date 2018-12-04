a86c71fc2e1b7562599227b90355804ce042e488
David Sanchez <csdavidg@hotmail.com>
2018-12-04 14:27:46 -0500

    Se carga ejemplo completo de la construccion propia del patron observable

--- src/main/java/co/com/observer/WeatherStation.java
+++ src/main/java/co/com/observer/WeatherStation.java
@@ -1,4 +1,27 @@
 package co.com.observer;
 
+import co.com.observer.display.CurrentConditionsDisplay;
+import co.com.observer.display.ForecastDisplay;
+import co.com.observer.display.HeatIndexDisplay;
+import co.com.observer.display.StatisticsDisplay;
+import co.com.observer.subjects.WeatherData;
+
 public class WeatherStation {
+
+    public static void main(String args[]) {
+
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4F);
+        weatherData.setMeasurements(82, 70, 29.2F);
+        weatherData.setMeasurements(78, 90, 29.2F);
+
+
+    }
+
 }
--- src/main/java/co/com/observer/display/CurrentConditionsDisplay.java
+++ src/main/java/co/com/observer/display/CurrentConditionsDisplay.java
@@ -1,23 +1,27 @@
-package headfirst.designpatterns.observer.weather;
-	
+package co.com.observer.display;
+
+import co.com.observer.interfaces.DisplayElement;
+import co.com.observer.interfaces.Observer;
+import co.com.observer.interfaces.Subject;
+
 public class CurrentConditionsDisplay implements Observer, DisplayElement {
-	private float temperature;
-	private float humidity;
-	private Subject weatherData;
-	
-	public CurrentConditionsDisplay(Subject weatherData) {
-		this.weatherData = weatherData;
-		weatherData.registerObserver(this);
-	}
-	
-	public void update(float temperature, float humidity, float pressure) {
-		this.temperature = temperature;
-		this.humidity = humidity;
-		display();
-	}
-	
-	public void display() {
-		System.out.println("Current conditions: " + temperature 
-			+ "F degrees and " + humidity + "% humidity");
-	}
+    private float temperature;
+    private float humidity;
+    private Subject weatherData;
+
+    public CurrentConditionsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+
+    public void display() {
+        System.out.println("Current conditions: " + temperature
+                + "F degrees and " + humidity + "% humidity");
+    }
 }
